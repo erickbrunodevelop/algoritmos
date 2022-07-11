403467ff2d56b3cd5a5f0c8afc8f991f87fdd06c
erick <erick@DESKTOP-DBCO23I>
2022-07-11 09:16:20 -0300

    ALTERADO

--- Algoritmos/src/Teste.java
+++ Algoritmos/src/Teste.java
@@ -1,8 +1,11 @@
+import java.util.Date;
 
 public class Teste {
 	public static void main(String[] args) {
 
 		String gitHub = "github";
-		System.out.println("ola " + gitHub);
+		System.out.println("ola " + gitHub + new Date().getTime());
+		
+		
 	}
 }
